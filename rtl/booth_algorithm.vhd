8db36551a31d398bb0ff83b993791ba6a2159610
Diskouna GNANGUESSIM <gnanguessim@insa-toulouse.fr>
2024-12-22 20:51:43 +0100

    Fix testcase 7 (a_i = MIN)

--- rtl/booth_algorithm.vhd
+++ rtl/booth_algorithm.vhd
@@ -23,14 +23,14 @@ entity booth_algorithm is
 end entity;
 
 architecture behavioral of booth_algorithm is 
-    type state_t is (IDLE, ONES_START, ONES_OR_ZEROS_STREAM, ZEROS_START, DONE); 
+    type state_t is (IDLE, START, ONES_START, ONES_OR_ZEROS_STREAM, ZEROS_START, DONE); 
     signal cur_state, nxt_state : state_t;
 
-    signal ext_pos_a : std_logic_vector(A_BITS+B_BITS downto 0); 
-    signal ext_neg_a : std_logic_vector(A_BITS+B_BITS downto 0); 
+    signal ext_pos_a : std_logic_vector(A_BITS+B_BITS+1 downto 0); 
+    signal ext_neg_a : std_logic_vector(A_BITS+B_BITS+1 downto 0); 
     
     -- Accumulator
-    signal accumulator : std_logic_vector(A_BITS+B_BITS downto 0); 
+    signal accumulator : std_logic_vector(A_BITS+B_BITS+1 downto 0); 
     signal load_accumulator  : std_logic := '0';
     signal shift_accumulator : std_logic := '0';
     signal add_multiplicand  : std_logic := '0';
@@ -47,16 +47,16 @@ begin
 
     -- DATAPATH
 
-    ext_pos_a(A_BITS+B_BITS downto B_BITS+1) <= a_i;
+    ext_pos_a(A_BITS+B_BITS+1 downto B_BITS+1) <= a_i(a_i'left) & a_i;
     ext_pos_a(B_BITS downto 0) <= (others => '0');
 
-    ext_neg_a(A_BITS+B_BITS downto B_BITS+1) <= std_logic_vector(-signed(a_i));
+    ext_neg_a(A_BITS+B_BITS+1 downto B_BITS+1) <= std_logic_vector(-signed(a_i(a_i'left) & a_i));
     ext_neg_a(B_BITS downto 0) <= (others => '0');
  
     c_o <= accumulator(A_BITS+B_BITS downto 1);
 
     ACCUMULATOR_PROCESS : process (clk_i)
-        variable accumulator_tmp : std_logic_vector(A_BITS+B_BITS downto 0) 
+        variable accumulator_tmp : std_logic_vector(A_BITS+B_BITS+1 downto 0) 
                                  := (others => '0');
     begin
         if (rising_edge(clk_i)) then
@@ -64,7 +64,7 @@ begin
                 accumulator <= (others => '0');
             elsif (load_accumulator = '1') then
                 accumulator(B_BITS downto 0) <= b_i & '0';
-                accumulator(A_BITS+B_BITS downto B_BITS+1) <= (others => '0'); 
+                accumulator(A_BITS+B_BITS+1 downto B_BITS+1) <= (others => '0'); 
             elsif (shift_accumulator = '1') then
                 if   (add_multiplicand = '1') then
                     accumulator_tmp := std_logic_vector(unsigned(accumulator) 
@@ -94,14 +94,13 @@ begin
         end if;
     end process;
 
-    end_of_loop <= '1' when (unsigned(counter) = B_BITS) else
+    end_of_loop <= '1' when (unsigned(counter) = B_BITS-1) else
                    '0';
 
-    -- one bit comparaison should be enough ...
-    start_of_ones <= '1' when (accumulator(1 downto 0) = "10") else
-                     '0';
-    start_of_zeros   <= '1' when (accumulator(1 downto 0) = "01") else
-                     '0';
+    start_of_ones  <= '1' when (accumulator(1 downto 0) = "10") else
+                      '0';
+    start_of_zeros <= '1' when (accumulator(1 downto 0) = "01") else
+                      '0';
     -- FSM
     
     STATE_TRAN_PROCESS : process (cur_state, a_b_valid_i, end_of_loop, 
@@ -121,54 +120,74 @@ begin
                 load_accumulator <= '1';
                 init_counter     <= '1';
                 if (a_b_valid_i = '1') then
-                    nxt_state <= ZEROS_START;
+                    nxt_state <= ONES_OR_ZEROS_STREAM; 
                 end if;
-
-            when ONES_START =>
-                if (end_of_loop = '1') then
-                    nxt_state <= DONE;
-                else
+            
+            when ONES_OR_ZEROS_STREAM =>
                     incr_counter      <= '1';
                     shift_accumulator <= '1';
-                    if (start_of_zeros = '1') then
+                    if    (start_of_ones  = '1') then -- 10
+                        sub_multiplicand <= '1';
+                        if (end_of_loop = '1') then
+                            nxt_state <= DONE;
+                        else
+                            nxt_state <= ONES_START;
+                        end if;
+                    elsif (start_of_zeros = '1') then -- 01
                         add_multiplicand <= '1';
+                        if (end_of_loop = '1') then
+                            nxt_state <= DONE;
+                        else
+                            nxt_state <= ZEROS_START;
+                        end if;
+                    else                              -- 00 / 11
+                        if (end_of_loop = '1') then
+                            nxt_state <= DONE;
+                        else
+                            nxt_state <= ONES_OR_ZEROS_STREAM;
+                        end if;
+                    end if; 
+
+            when ONES_START =>
+                incr_counter      <= '1';
+                shift_accumulator <= '1';
+                if (start_of_zeros = '1') then  -- 01
+                    add_multiplicand <= '1';
+                    if (end_of_loop = '1') then
+                        nxt_state <= DONE;
+                    else
                         nxt_state <= ZEROS_START;
+                    end if;
+                else                            -- 11
+                    if (end_of_loop = '1') then
+                        nxt_state <= DONE;
                     else
                         nxt_state <= ONES_OR_ZEROS_STREAM;
                     end if;
                 end if;
-            when ONES_OR_ZEROS_STREAM =>
-                if (end_of_loop = '1') then
-                    nxt_state <= DONE;
-                else
-                    incr_counter      <= '1';
-                    shift_accumulator <= '1';
-                    if (start_of_ones = '1') then
-                        sub_multiplicand <= '1';
-                        nxt_state <= ONES_START;
-                    elsif (start_of_zeros = '1') then
-                        add_multiplicand <= '1';
-                        nxt_state <= ZEROS_START;
-                    else 
-                        nxt_state <= ONES_OR_ZEROS_STREAM;
-                    end if; 
-                end if;
+
             when ZEROS_START =>
-                if (end_of_loop = '1') then
-                    nxt_state <= DONE;
-                else
-                    incr_counter      <= '1';
-                    shift_accumulator <= '1';
-                    if (start_of_ones = '1') then
-                        sub_multiplicand <= '1';
+                incr_counter      <= '1';
+                shift_accumulator <= '1';
+                if (start_of_ones = '1') then   -- 10
+                    sub_multiplicand <= '1';
+                    if (end_of_loop = '1') then
+                        nxt_state <= DONE;
+                    else
                         nxt_state <= ONES_START;
-                    else 
+                    end if;
+                else                            -- 00
+                    if (end_of_loop = '1') then
+                        nxt_state <= DONE;
+                    else
                         nxt_state <= ONES_OR_ZEROS_STREAM;
-                    end if; 
-                end if;
+                    end if;
+                end if; 
+
             when DONE =>
                 c_valid_o <= '1';
                 nxt_state <= IDLE;
+
             when others =>
                 nxt_state <= IDLE;
         end case;
