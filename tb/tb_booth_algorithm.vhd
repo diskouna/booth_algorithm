f93fda3d51b79f302dedcfa765edf90910d6f4ca
Diskouna GNANGUESSIM <gnanguessim@insa-toulouse.fr>
2024-12-26 09:54:39 +0100

    Use i/o interfaces instead of individual signals in BFM procedures

--- tb/tb_booth_algorithm.vhd
+++ tb/tb_booth_algorithm.vhd
@@ -25,16 +25,8 @@ architecture behavioral of tb_booth_algorithm is
     
     signal clk   : std_logic := '0';
     signal rst   : std_logic := '1';
-	signal a_b_valid   : std_logic;
-	signal prod_ready  : std_logic;
-	signal a, b        : std_logic_vector (7 downto 0);        
-	signal c_valid     : std_logic;
-	signal cons_ready  : std_logic := '1'; -- output consumer is always ready
-	signal c           : std_logic_vector (15 downto 0); 
-
-    -- TODO: use booth_algorithm core interface
+    
     type booth_if_t is record
-        clk_i, rst_i    : std_logic;
         a_b_valid_i     : std_logic;
         prod_ready_o    : std_logic;
         a_i, b_i        : std_logic_vector (7 downto 0);        
@@ -42,35 +34,34 @@ architecture behavioral of tb_booth_algorithm is
         cons_ready_i    : std_logic;
         c_o             : std_logic_vector (15 downto 0); 
     end record;
-    signal  booth_if : booth_if_t;
+
+    signal  booth_if_in, booth_if_out : booth_if_t;
 
     -- BFM procedures    
-    procedure set_operands(signal clk : in std_logic;
-                           signal a, b : out std_logic_vector;
-                           signal a_b_valid : out std_logic;
-                           signal pro_ready : in std_logic;
-                           constant a_value, b_value : in    integer) is
+    procedure set_operands(signal clk        : in std_logic;
+                           signal core_if_out: in booth_if_t;
+                           signal core_if_in : out booth_if_t;
+                           constant a_value, b_value : in  integer) is
     begin
-        a <= std_logic_vector(to_signed(a_value, a'length));
-        b <= std_logic_vector(to_signed(b_value, b'length));
-        a_b_valid <= '1';
-        wait until rising_edge(clk) and prod_ready = '1'; 
+        core_if_in.a_i <= std_logic_vector(to_signed(a_value, core_if_in.a_i'length));
+        core_if_in.b_i <= std_logic_vector(to_signed(b_value, core_if_in.b_i'length));
+        core_if_in.a_b_valid_i <= '1';
+        wait until rising_edge(clk) and core_if_out.prod_ready_o = '1';
         wait until rising_edge(clk);
-        a_b_valid <= '0';
+        core_if_in.a_b_valid_i <= '0';
         report "Set a=" & integer'image(a_value) & " and b=" & integer'image(b_value)
                severity note; 
     end procedure; 
 
-    procedure get_result(signal clk : in std_logic;
-                         signal c : in std_logic_vector;
-                         signal c_valid    : in std_logic;
-                         signal cons_ready : in std_logic;
+    procedure get_result(signal clk        : in  std_logic;
+                         signal core_if_out: in  booth_if_t;
+                         signal core_if_in : out booth_if_t;
                          variable c_value  : out integer) is
     begin
-        wait until rising_edge(clk)  and c_valid = '1' and 
-                   cons_ready = '1' ;
-        c_value := to_integer(signed(c));
-        report "Got c=" & integer'image(to_integer(signed(c))) severity note; 
+        wait until rising_edge(clk)  and core_if_out.c_valid_o = '1'; 
+                   -- and core_if_in.cons_ready_i = '1' ;
+        c_value := to_integer(signed(core_if_out.c_o));
+        report "Got c=" & integer'image(to_integer(signed(core_if_out.c_o))) severity note; 
     end procedure; 
 
     -- Checking procedure
@@ -89,13 +80,13 @@ begin
                           port    map (
                                 clk_i        => clk,
                                 rst_i        => rst,
-                                a_b_valid_i  => a_b_valid,
-                                prod_ready_o => prod_ready,   
-                                a_i          => a,
-                                b_i          => b,
-                                c_valid_o    => c_valid,
-                                cons_ready_i => cons_ready,
-                                c_o          => c
+                                a_b_valid_i  => booth_if_in.a_b_valid_i,
+                                prod_ready_o => booth_if_out.prod_ready_o,   
+                                a_i          => booth_if_in.a_i,
+                                b_i          => booth_if_in.b_i,
+                                c_valid_o    => booth_if_out.c_valid_o,
+                                cons_ready_i => booth_if_in.cons_ready_i,
+                                c_o          => booth_if_out.c_o
                           );
 
     -- clock generation
@@ -111,54 +102,56 @@ begin
         rst <= '0';  
         report "Reset generation done" severity note;
         
+        booth_if_in.cons_ready_i <= '1'; -- output consumer is always ready
+        
         -- Test case 1 : Two positive numbers    
-        set_operands(clk, a, b, a_b_valid, prod_ready, 30, 42);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, 30, 42);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 1260, test_case_id => 1);
         
         -- Test case 2 : Two negative numbers
-        set_operands(clk, a, b, a_b_valid, prod_ready, -3, -4);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, -3, -4);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 12, test_case_id => 2);
 
         -- Test case 3 : Two negative numbers (bis)
-        set_operands(clk, a, b, a_b_valid, prod_ready, -30, -42);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, -30, -42);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 1260, test_case_id => 3);
         
         -- Test case 4 : Positive and Negative numbers
-        set_operands(clk, a, b, a_b_valid, prod_ready, -30, 42);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, -30, 42);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => -1260, test_case_id => 4);
 
         -- Test case 5 : Zeros
-        set_operands(clk, a, b, a_b_valid, prod_ready, 0, 0);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, 0, 0);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 0, test_case_id => 5);
         
         -- Test case 6 : Min and Max
-        set_operands(clk, a, b, a_b_valid, prod_ready, 127, -128);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, 127, -128);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => -16256, test_case_id => 6);
         
         -- Test case 7 : Min and Min
-        set_operands(clk, a, b, a_b_valid, prod_ready, -128, -128);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, -128, -128);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 16384 , test_case_id => 7);
 
         -- Test case 8 : Max and Max
-        set_operands(clk, a, b, a_b_valid, prod_ready, 127, 127);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, 127, 127);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 16129, test_case_id => 8);
         
         -- Test case 9 : Zero and Max
-        set_operands(clk, a, b, a_b_valid, prod_ready, 0, 127);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, 0, 127);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 0, test_case_id => 9);
 
         -- Test case 10 : Zero and Min
-        set_operands(clk, a, b, a_b_valid, prod_ready, 0, -128);
-        get_result  (clk, c, c_valid, cons_ready, actual_c);
+        set_operands(clk, booth_if_out, booth_if_in, 0, -128);
+        get_result  (clk, booth_if_out, booth_if_in, actual_c);
         check_result(result => actual_c, expected_result => 0, test_case_id => 10);
 
         report "Simulation completed" severity note;
